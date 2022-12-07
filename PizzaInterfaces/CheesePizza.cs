e8400c7ae25d68a77c80fb558e6163b985382525
elzzzbieta <elzbieta.petkevic@gmail.com>
2022-12-07 20:17:21 +0200

    Add project files.

---
+++ PizzaInterfaces/CheesePizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaInterfaces
+{
+    public class CheesePizza : IPizza
+    {
+        public void Prepare()
+        {
+            Console.WriteLine("Preparing cheese pizza");
+        }
+    }
+}
---
+++ PizzaInterfaces/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var order  = new List<IPizza> { new NewYorkPizza(), new CheesePizza() };
+            order.ForEach(pizza => pizza.Prepare());
+        }
+    }
+}
